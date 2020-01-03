129261034b584ef02e83d63bc87609c84d115952
Daniel McCrady <dan.mccrady@focisolutions.com>
2020-01-03 11:21:46 -0500

    Initial Commit

---
+++ src/demo-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
